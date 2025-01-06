bb9026ea2aa191646248d568cbc15a5d18ece328
Kenneth Raknes <raknes@hotmail.com>
2025-01-06 10:36:52 +0100

    shut down

---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+        <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li>Aller AI Platform har fått et nytt hjem</li>
+          <li className="mb-2">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+                https://platform.ai.aller.com/
+              </code>
+            </a>
+          </li>
+        </ul>
+
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          href="https://aller.mediehub.net"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="sr-only">Powered by Mediehub</span>
+          <Image
+            src="/mediehub.png"
+            alt="Vercel Logo"
+            width={72}
+            height={16}
+          />
+        </a>
+      </footer>
+    </div>
+  );
+}
